d9d5b9c2582cb56419539230d26b7fe77ca419b1
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-08-26 23:13:04 +0300

    1

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB =
+import * as path from 'node:path';
+
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
+
+// const path1 = path.resolve('src', 'db', 'db.json');
+// console.log(path1);
+
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,17 @@
-import { PATH_DB } from '../constants/contacts.js';
+import {PATH_DB}   from '../constants/contacts.js';
+import { createFakeContact } from "../utils/createFakeContact.js";
+import * as fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {};
+
+
+
+const generateContacts = async (number) => {
+    const songList = await fs.readFile(PATH_DB, 'utf-8');
+    console.log(songList);
+
+    // const newContactsList = Array(number).fill(0).map(() => createFakeContact());
+    // // console.log(newContactsList);
+    // await fs.writeFile(PATH_DB, JSON.stringify(newContactsList, null, 2));
+};
 
 generateContacts(5);
