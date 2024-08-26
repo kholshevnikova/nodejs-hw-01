5080885dddf66de886290bf37b5ab65b9d8d10fb
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-08-27 00:51:59 +0300

    2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,17 +1,20 @@
-import {PATH_DB}   from '../constants/contacts.js';
+// import {PATH_DB}   from '../constants/contacts.js';
 import { createFakeContact } from "../utils/createFakeContact.js";
-import * as fs from 'node:fs/promises';
+// import * as fs from 'node:fs/promises';
+// import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
+import getAllContacts from './getAllContacts.js';
+import updateContacts from './updateContacts.js';
 
 
 
 
 const generateContacts = async (number) => {
-    const songList = await fs.readFile(PATH_DB, 'utf-8');
-    console.log(songList);
 
-    // const newContactsList = Array(number).fill(0).map(() => createFakeContact());
-    // // console.log(newContactsList);
-    // await fs.writeFile(PATH_DB, JSON.stringify(newContactsList, null, 2));
+    // const {encoding} = await DetectFileEncodingAndLanguage(PATH_DB);
+    const contactList = await getAllContacts();
+    const newContactsList = Array(number).fill(0).map(createFakeContact);
+    contactList.push(...newContactsList);
+    await updateContacts(contactList);
 };
 
 generateContacts(5);
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+const getAllContacts = async () => {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(data);
 
+};
+export default getAllContacts;
 console.log(await getAllContacts());
+
